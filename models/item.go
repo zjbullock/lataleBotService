d4871763607fcc4a9007c6f23742894bbc363e83
Zee <zjbulllock@gmail.com>
2021-07-11 16:22:44 -0400

    Adding more ascension level related things

--- models/item.go
+++ models/item.go
@@ -1,16 +1,17 @@
 package models
 
 type Item struct {
-	Name             string        `json:"name" firestore:"name"`
-	Type             ItemType      `json:"type" firestore:"type"`
-	LevelRequirement *float64      `json:"levelRequirement,omitempty" firestore:"levelRequirement,omitempty"`
-	Shop             bool          `json:"shop" firestore:"shop"`
-	Description      *string       `json:"description,omitempty" firestore:"description,omitempty"`
-	Cost             *int32        `json:"cost" firestore:"cost"`
-	Stats            *StatModifier `json:"stats" firestore:"stats"`
-	Boss             *string       `json:"boss" firestore:"boss"`
-	SetBonusId       *string       `json:"setBonusId" firestore:"setBonusId"`
-	RequiredClasses  *[]*string    `json:"requiredClasses" firestore:"requiredClasses"`
+	Name                      string        `json:"name" firestore:"name"`
+	Type                      ItemType      `json:"type" firestore:"type"`
+	LevelRequirement          *float64      `json:"levelRequirement,omitempty" firestore:"levelRequirement,omitempty"`
+	AscensionLevelRequirement *float64      `json:"ascensionLevelRequirement,omitempty" firestore:"ascensionLevelRequirement,omitempty"`
+	Shop                      bool          `json:"shop" firestore:"shop"`
+	Description               *string       `json:"description,omitempty" firestore:"description,omitempty"`
+	Cost                      *int32        `json:"cost" firestore:"cost"`
+	Stats                     *StatModifier `json:"stats" firestore:"stats"`
+	Boss                      *string       `json:"boss" firestore:"boss"`
+	SetBonusId                *string       `json:"setBonusId" firestore:"setBonusId"`
+	RequiredClasses           *[]*string    `json:"requiredClasses" firestore:"requiredClasses"`
 }
 
 type InventoryItem struct {
